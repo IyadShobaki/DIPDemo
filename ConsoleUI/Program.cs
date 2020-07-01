bd6440514bfeb26d7f0e86ed0ac6a3c041f4d3fe
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-30 22:48:25 -0400

    The Code After Applying DIP

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,35 +1,31 @@
 ﻿using DemoLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 // DIP stands for "Dependency Inversion Principle" design pattern
 // DIP states that high-level modules should not depend on low-level modules
 // But rather both should depend on abstractions and those abstractions
 // should not depend on details.
+// Dependency Inversion is the principle and Dependency Injection is one of the ways
+// (the most common way) where you make the principle work
 namespace ConsoleUI
 {
-    class Program
+    internal class Program
     {
-        // Main is high-level depend on Person and Chore
-        // If we made changes to Chore will break Program.cs
-        static void Main(string[] args) 
+        private static void Main(string[] args)
         {
-            Person owner = new Person
-            {
-                FirstName = "Iyad",
-                LastName = "Shobaki",
-                EmailAddress = "I@Shobaki.com",
-                PhoneNumber = "555-1212"
-            };
+            IPerson owner = Factory.CreatePerson();
 
-            Chore chore = new Chore
-            {
-                ChoreName = "Take out the trash",
-                Owner = owner
-            };
+            owner.FirstName = "Iyad";
+            owner.LastName = "Shobaki";
+            owner.EmailAddress = "I@Shobaki.com";
+            owner.PhoneNumber = "555-1212";
+
+
+            IChore chore = Factory.CreateChore();
+
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+            
 
             chore.PerformedWork(3);
             chore.PerformedWork(1.5);
--- DemoLibrary/Chore.cs
+++ DemoLibrary/Chore.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    // Chore is high-level depend on Logger and Emailer
-    // If we made change to Logger will break the Chore and will break Program.cs
-    public class Chore
+    public class Chore : IChore
     {
+        ILogger _logger;
+        IMessageSender _messageSender;
+
         public string ChoreName { get; set; }
-        public Person Owner { get; set; }
+        public IPerson Owner { get; set; }
         public double HoursWorked { get; private set; }
         public bool IsComplete { get; private set; }
+
+        public Chore(ILogger logger, IMessageSender messageSender)
+        {
+            _logger = logger;
+            _messageSender = messageSender;
+        }
 
         public void PerformedWork(double hours)
         {
@@ -25,11 +32,10 @@ namespace DemoLibrary
         public void CompleteChore()
         {
             IsComplete = true;
-            Logger log = new Logger();
-            log.Log($"Competed { ChoreName }");
 
-            Emailer emailer = new Emailer();
-            emailer.SendEmail(Owner, $"The chore { ChoreName } is complete.");
+            _logger.Log($"Competed { ChoreName }");
+
+            _messageSender.SendMessage(Owner, $"The chore { ChoreName } is complete.");
         }
     }
 }
